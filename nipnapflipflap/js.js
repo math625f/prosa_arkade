3899223ed5b8707b1c9456b2d0827d1f75aff2f6
Mathias Rasmussen <mras@sdu.dk>
2022-04-22 17:21:03 +0200

    Finished Nip nap flip flap

--- nipnapflipflap/js.js
+++ nipnapflipflap/js.js
@@ -1,6 +1,23 @@
+let step = 1;
+let animationDuration = 350;
+
 $(document).ready(() => {
-    $(document).on('click', () => {
-        toggle();
+    $(".top, .north, .south, .west, .east").on('click', (el) => {
+        if(step == 1){
+            $("#container").removeClass('base');
+        }
+        
+        if(step == 4){
+            if(!el.target.classList.contains('top')){
+                alert($(el.target).attr('data-reward'));
+            }
+        }else{
+            if(el.target.classList.contains('top')){
+                doStep(Number(el.target.innerText));
+            }else{
+                doStep(el.target.innerText.length);
+            }
+        }
     })
 });
 
@@ -11,6 +28,12 @@ function toggle(){
     $("#container").removeClass(old);
     setTimeout(() => {
         $("#container").addClass(now);
-    }, 500);
+    }, animationDuration);
+}
 
+function doStep(n){
+    for(let i = 0; i < n; i++){
+        setTimeout(toggle, i * animationDuration * 2);
+    }
+    step++;
 }
